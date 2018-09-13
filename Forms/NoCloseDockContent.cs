9b51c843e4cecfe5716f8eeb848f7a84074ad162
Damien Gibson <spiceywolf.archaicsoft@outlook.com>
2018-09-13 17:00:57 -0400

    Added new base DockContent type "NoCloseDockContent
    This new type was added to simplify the process of trying to make dock content that cant be nulled out -> Useful for editors that may contain localized class objects and scanners like in game engines.

---
+++ Forms/NoCloseDockContent.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Windows.Forms
+{
+    public class NoCloseDockContent : DockContent
+    {
+        public NoCloseDockContent()
+        {
+            FormClosing += (sender, e) =>
+            {
+                DockState = DockState.Hidden;
+                e.Cancel = true;
+            };
+        }
+    }
+}
